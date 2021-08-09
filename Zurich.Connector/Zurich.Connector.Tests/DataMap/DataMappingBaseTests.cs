f82af4ae9c90b2a78fb4823d00491c9c88845fbc
Ryan Hunecke <ryan.hunecke@thomsonreuters.com>
2021-08-09 16:14:07 +0000

    Merged PR 9011: Updating to use beta endpoint
    Update to use beta endpoint and fix an issue with multiple params coming in
    
    Related work items: #43666

--- Zurich.Connector/Zurich.Connector.Tests/DataMap/DataMappingBaseTests.cs
+++ Zurich.Connector/Zurich.Connector.Tests/DataMap/DataMappingBaseTests.cs
@@ -29,71 +29,155 @@ namespace Zurich.Connector.Tests
 		}
 	}
 
-	[TestClass]
-	public class DataMappingBaseTests
-	{
-		private Mock<IRepository> _mockRepository;
-		private Mock<IDataMappingRepository> _mockDataMappingRepository;
-		private Mock<IOAuthService> _mockOAuthService;
-		private Mock<ILogger<DataMappingOAuth>> _mockLoggerOAuth;
-		private Mock<ILogger<DataMappingTransfer>> _mockLoggerTransfer;
-		private Mock<ICosmosClientStore> _mockCosmosDocumentReader;
-		private Mock<IMapper> _mockMapper;
+    [TestClass]
+    public class DataMappingBaseTests
+    {
+        private Mock<IRepository> _mockRepository;
+        private Mock<IDataMappingRepository> _mockDataMappingRepository;
+        private Mock<IOAuthService> _mockOAuthService;
+        private Mock<ILogger<DataMappingOAuth>> _mockLoggerOAuth;
+        private Mock<ILogger<DataMappingTransfer>> _mockLoggerTransfer;
+        private Mock<ICosmosClientStore> _mockCosmosDocumentReader;
+        private Mock<IMapper> _mockMapper;
 
-		[TestInitialize]
-		public void TestInitialize()
-		{
-			_mockRepository = new Mock<IRepository>();
-			_mockDataMappingRepository = new Mock<IDataMappingRepository>();
-			_mockOAuthService = new Mock<IOAuthService>();
-			_mockLoggerOAuth = new Mock<ILogger<DataMappingOAuth>>();
-			_mockLoggerTransfer = new Mock<ILogger<DataMappingTransfer>>();
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IRepository>();
+            _mockDataMappingRepository = new Mock<IDataMappingRepository>();
+            _mockOAuthService = new Mock<IOAuthService>();
+            _mockLoggerOAuth = new Mock<ILogger<DataMappingOAuth>>();
+            _mockLoggerTransfer = new Mock<ILogger<DataMappingTransfer>>();
 
-			// feels like this won't change
-			AppToken token = new AppToken() { access_token = "fakeToken" };
-			_mockOAuthService.Setup(x => x.GetToken(It.IsAny<string>(), It.IsAny<OAuthApplicationType>(), It.IsAny<bool>(), It.IsAny<string>(), It.IsAny<ProductType>())).Returns(Task.FromResult(token));
-			_mockCosmosDocumentReader = new Mock<ICosmosClientStore>();
-			_mockMapper = new Mock<IMapper>();
-		}
+            // feels like this won't change
+            AppToken token = new AppToken() { access_token = "fakeToken" };
+            _mockOAuthService.Setup(x => x.GetToken(It.IsAny<string>(), It.IsAny<OAuthApplicationType>(), It.IsAny<bool>(), It.IsAny<string>(), It.IsAny<ProductType>())).Returns(Task.FromResult(token));
+            _mockCosmosDocumentReader = new Mock<ICosmosClientStore>();
+            _mockMapper = new Mock<IMapper>();
+        }
 
-		[TestMethod]
-		public void TestMapping()
-		{
-			// ARRANGE
-			TestDataMapping dataMapping = new TestDataMapping(_mockRepository.Object, _mockDataMappingRepository.Object, _mockOAuthService.Object, _mockLoggerOAuth.Object, _mockCosmosDocumentReader.Object, _mockMapper.Object);
+        [TestMethod]
+        public void TestMapping()
+        {
+            // ARRANGE
+            TestDataMapping dataMapping = new TestDataMapping(_mockRepository.Object, _mockDataMappingRepository.Object, _mockOAuthService.Object, _mockLoggerOAuth.Object, _mockCosmosDocumentReader.Object, _mockMapper.Object);
 
-			ConnectorDocument document = new ConnectorDocument()
-			{
-				request = new ConnectorRequest()
+            ConnectorDocument document = new ConnectorDocument()
+            {
+                request = new ConnectorRequest()
                 {
-					parameters = new List<ConnectorRequestParameter>()
+                    parameters = new List<ConnectorRequestParameter>()
                     {
-						new ConnectorRequestParameter() { 
-							cdmname = "Query",
-							//name = "requests.query.queryString",
-							name = "requests.[].query.queryString",
-							inClause = "body"
-						},
-						new ConnectorRequestParameter() {
-							cdmname = "EntityType",
-							//name = "requests.entityTypes",
-							name = "requests.[].entityTypes.[]",
-							inClause = "body"
-						}
-					}
+                        new ConnectorRequestParameter() {
+                            cdmname = "Query",
+                            name = "requests.[].query.queryString",
+                            inClause = "body"
+                        },
+                        new ConnectorRequestParameter() {
+                            cdmname = "EntityType",
+                            name = "requests.[].entityTypes.[]",
+                            inClause = "body"
+                        }
+                    }
                 }
-			};
+            };
 
-			NameValueCollection collection = new NameValueCollection();
-			collection["requests.[].entityTypes.[]"] = "driveItems";
-			collection["requests.[].query.queryString"] = "docs";
-				
-			// ACT
-			var postBody = dataMapping.CreatePostBody(document, collection);
+            NameValueCollection collection = new NameValueCollection();
+            collection["requests.[].entityTypes.[]"] = "driveItems";
+            collection["requests.[].query.queryString"] = "docs";
 
-			// ASSERT
-			Assert.AreEqual("{\"requests\":[{\"query\":{\"queryString\":\"docs\"},\"entityTypes\":[\"driveItems\"]}]}", postBody);
-		}
+            // ACT
+            var postBody = dataMapping.CreatePostBody(document, collection);
 
-	}
+            // ASSERT
+            Assert.AreEqual("{\"requests\":[{\"query\":{\"queryString\":\"docs\"},\"entityTypes\":[\"driveItems\"]}]}", postBody);
+        }
+
+        [TestMethod]
+        public void TestMappingMultiParam()
+        {
+            // ARRANGE
+            TestDataMapping dataMapping = new TestDataMapping(_mockRepository.Object, _mockDataMappingRepository.Object, _mockOAuthService.Object, _mockLoggerOAuth.Object, _mockCosmosDocumentReader.Object, _mockMapper.Object);
+
+            ConnectorDocument document = new ConnectorDocument()
+            {
+                request = new ConnectorRequest()
+                {
+                    parameters = new List<ConnectorRequestParameter>()
+                    {
+                        new ConnectorRequestParameter() {
+                            cdmname = "Query",
+                            name = "requests.[].query.queryString",
+                            inClause = "body"
+                        },
+                        new ConnectorRequestParameter() {
+                            cdmname = "EntityType",
+                            name = "requests.[].entityTypes.[]",
+                            inClause = "body"
+                        },
+                        new ConnectorRequestParameter() {
+                            cdmname = "Fields",
+                            name = "requests.[].fields.[]",
+                            inClause = "body"
+                        }
+                    }
+                }
+            };
+
+            NameValueCollection collection = new NameValueCollection();
+            collection["requests.[].entityTypes.[]"] = "driveItems";
+            collection["requests.[].query.queryString"] = "docs";
+            collection["requests.[].fields.[]"] = "Name,CreatedDateTime,LastModifiedDateTime,LastModifiedBy,WebUrl,ParentReference";
+
+            // ACT
+            var postBody = dataMapping.CreatePostBody(document, collection);
+
+            // ASSERT
+            Assert.AreEqual("{\"requests\":[{\"query\":{\"queryString\":\"docs\"},\"entityTypes\":[\"driveItems\"],\"fields\":[\"Name\",\"CreatedDateTime\",\"LastModifiedDateTime\",\"LastModifiedBy\",\"WebUrl\",\"ParentReference\"]}]}", postBody);
+        }
+
+        [TestMethod]
+        public void TestMappingNullValues()
+        {
+            // ARRANGE
+            TestDataMapping dataMapping = new TestDataMapping(_mockRepository.Object, _mockDataMappingRepository.Object, _mockOAuthService.Object, _mockLoggerOAuth.Object, _mockCosmosDocumentReader.Object, _mockMapper.Object);
+
+            ConnectorDocument document = new ConnectorDocument()
+            {
+                request = new ConnectorRequest()
+                {
+                    parameters = new List<ConnectorRequestParameter>()
+                    {
+                        new ConnectorRequestParameter() {
+                            cdmname = "Query",
+                            name = "requests.[].query.queryString",
+                            inClause = "body"
+                        },
+                        new ConnectorRequestParameter() {
+                            cdmname = "EntityType",
+                            name = "requests.[].entityTypes.[]",
+                            inClause = "body"
+                        },
+                        new ConnectorRequestParameter() {
+                            cdmname = "Fields",
+                            name = "requests.[].fields.[]",
+                            inClause = "body"
+                        }
+                    }
+                }
+            };
+
+            NameValueCollection collection = new NameValueCollection();
+            collection["requests.[].entityTypes.[]"] = "driveItems";
+            collection["requests.[].query.queryString"] = "docs";
+            collection["requests.[].fields.[]"] = null;
+
+            // ACT
+            var postBody = dataMapping.CreatePostBody(document, collection);
+
+            // ASSERT
+            Assert.AreEqual("{\"requests\":[{\"query\":{\"queryString\":\"docs\"},\"entityTypes\":[\"driveItems\"],\"fields\":[]}]}", postBody);
+        }
+
+    }
 }
