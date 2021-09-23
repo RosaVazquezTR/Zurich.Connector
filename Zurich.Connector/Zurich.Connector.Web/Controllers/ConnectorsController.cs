8b44724438ff5617ba5b7e25c663ba4bdc0e7e8b
Jayesh Titame <Jayesh.Titame@thomsonreuters.com>
2021-09-23 14:03:33 +0000

    Revert "Merged PR 9331: story 45153 setup versioning for connector
    Move existing Controllers under a V1 sub-folder
    Import Microsoft.AspNetCore.Mvc.Versioning and Microsoft.AspNetCore.Mvc.Versioning.ApiExplorer NuGet packages to our web project
    Add the attribute [ApiVersion(“1.0”)] to all our controllers
    Routes should be updated to include the literal string v{version:apiVersion}
    
    Related work items: #46650"

--- Zurich.Connector/Zurich.Connector.Web/Controllers/ConnectorsController.cs
+++ Zurich.Connector/Zurich.Connector.Web/Controllers/ConnectorsController.cs
@@ -21,8 +21,6 @@ namespace Zurich.Connector.Web.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    [ApiVersion("1.0")]
     public class ConnectorsController : ControllerBase
     {
         private readonly IConnectorService _connectorService;
