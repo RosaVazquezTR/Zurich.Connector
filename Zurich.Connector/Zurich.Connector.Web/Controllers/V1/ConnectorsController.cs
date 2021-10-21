f9e8f523390baf0f2c8145bf057242f47e8a341f
Naga Jayakanth Battu <NagaJayakanth.Battu@thomsonreuters.com>
2021-10-21 12:37:12 -0500

    Bug - 47568 Connectors should return an error response message in case of any exceptions

--- Zurich.Connector/Zurich.Connector.Web/Controllers/V1/ConnectorsController.cs
+++ Zurich.Connector/Zurich.Connector.Web/Controllers/V1/ConnectorsController.cs
@@ -65,7 +65,12 @@ namespace Zurich.Connector.Web.Controllers
             }
             catch (Exception e)
             {
-                return new Exception(e.Message);
+                return new ContentResult
+                {
+                    Content = e.Message,
+                    ContentType = System.Net.Mime.MediaTypeNames.Application.Json,
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
             }
 
         }
