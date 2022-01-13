f5ffdc467ec06224822a4de67d9b427799ab5fe9
Naga Jayakanth Battu <NagaJayakanth.Battu@thomsonreuters.com>
2022-01-13 00:02:39 -0600

    Updating the RequiresNewToken attribute

--- Zurich.Connector/Zurich.Connector.App/Services/ConnectorService.cs
+++ Zurich.Connector/Zurich.Connector.App/Services/ConnectorService.cs
@@ -107,6 +107,7 @@ namespace Zurich.Connector.Data.Services
                 foreach (var connector in connectors.Where(connector => registeredDataSources.Select(x => x.AppCode).Contains(connector.DataSource.AppCode)))
                 {
                     connector.RegistrationStatus = RegistrationStatus.Registered;
+                    connector.DataSource.RequiresNewToken = registeredDataSources.Where(x => x.AppCode == connector.DataSource.AppCode).First().RequiresNewToken;
                     registeredConnectors.Add(connector);
                 }
                 // Can't stick this in the cosmos query because it is looking at connectors not datasources.
