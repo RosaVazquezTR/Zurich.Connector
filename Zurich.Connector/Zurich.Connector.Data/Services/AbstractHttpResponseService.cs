88a7a1ecf25c02d23d6daf8551840a14d62e56f6
Jose Faustinos <jose.faustinos@thomsonreuters.com>
2023-02-22 13:06:59 -0600

    Step: Manual validation. TODO: Complete flow

--- Zurich.Connector/Zurich.Connector.Data/Services/AbstractHttpResponseService.cs
+++ Zurich.Connector/Zurich.Connector.Data/Services/AbstractHttpResponseService.cs
@@ -10,6 +10,7 @@ using System.Collections.Specialized;
 using System.Net.Http;
 using Zurich.Common.Repositories;
 using Zurich.Common.Services;
+using Zurich.Common.Models.IHDocumentStorage;
 
 namespace Zurich.Connector.Data.Services
 {
@@ -22,12 +23,35 @@ namespace Zurich.Connector.Data.Services
         }
         public async virtual Task<JToken> GetJTokenResponse(string response, ConnectorResponse connectorResponse, string connectorId, Dictionary<string, string> requestParameter, NameValueCollection query, IHttpClientFactory httpClientFactory)
         {
-            //TODO: add a data source parameter to avoid using connector ids directly in the code
             if (!(connectorResponse?.UseJsonTransformation ?? false))
                 return JToken.Parse(response);
             else
             // TT transformation response
             {
+                if (connectorResponse?.UsePermissionsCheck ?? false)
+                {
+
+                    IHDocumentStorageRepository iHDocumentStorageRepository = new IHDocumentStorageRepository(httpClientFactory);
+                    IHDocumentStorageService iHDocumentStorageService = new IHDocumentStorageService(iHDocumentStorageRepository);
+
+                    Common.Models.IHDocumentStorage.IHDocumentStorageMappingRequest iHDocumentStorageMappingRequest = new IHDocumentStorageMappingRequest();
+                    List<string> ids = new List<string>();
+                    ids.Add("c00072ca-88e9-452f-9740-4d95b7ceca");
+                    ids.Add("a8f0e08e-c705-44d7-a514-a1d3741df9b9"); 
+                    iHDocumentStorageMappingRequest.ids = ids; 
+
+                    var mappings = iHDocumentStorageService.GetDocumentsMappings("ca353457-26ad-473e-ab46-aae01309a093", iHDocumentStorageMappingRequest); 
+
+                    IHDocumentStoragePermissionsRequest iHDocumentStoragePermissionsRequest = new IHDocumentStoragePermissionsRequest();
+                    iHDocumentStoragePermissionsRequest.appCode = "iManageServiceApp";
+                    List<string> documentIds = new List<string>();
+                    documentIds.Add("3f7f97be-aeb2-4907-982a-08db09326bf9");
+                    documentIds.Add("9935fede-d8ae-4668-b9a0-08db0afb19ac");
+                    documentIds.Add("13f31894-e169-41ef-982b-08db09326bf9"); iHDocumentStoragePermissionsRequest.documentIds = documentIds; 
+                    var permissions = iHDocumentStorageService.GetDocumentsPermissions(iHDocumentStoragePermissionsRequest);
+
+
+                }
                 string provisionID = query["provisionID"];
                 string input = "{\"Documents\":" + response + "}";
                 string path = Directory.GetCurrentDirectory() + "\\Transformation\\TTtransformer3.json";
@@ -89,14 +113,14 @@ namespace Zurich.Connector.Data.Services
                 if (requestParameter.ContainsKey("start") && (Convert.ToInt32(requestParameter["start"]) > 0))
                 {
                     currentPage = Convert.ToInt32(requestParameter["start"]);
-                    if (currentPage > totalPages) 
+                    if (currentPage > totalPages)
                     {
                         currentPage = totalPages;
                     }
                 }
-                
-                paginationDoc.from = (currentPage-1) * resultsPerPage+1;
-                paginationDoc.to = ((currentPage)*resultsPerPage> totalThoughts)? totalThoughts : (currentPage) * resultsPerPage;
+
+                paginationDoc.from = (currentPage - 1) * resultsPerPage + 1;
+                paginationDoc.to = ((currentPage) * resultsPerPage > totalThoughts) ? totalThoughts : (currentPage) * resultsPerPage;
                 paginationDoc.of = totalThoughts;
 
                 paginationDoc.PagesPaginationInfo = new JObject();
@@ -115,7 +139,7 @@ namespace Zurich.Connector.Data.Services
                 else
                     paginationDoc.PagesPaginationInfo.previous = currentPage - 1;
 
-                if(totalThoughts>0)
+                if (totalThoughts > 0)
                     jObjectTop.Add(new JProperty("pagination", paginationDoc));
                 jObjectTop.Add(new JProperty("totalDocs", totalThoughts));
 
