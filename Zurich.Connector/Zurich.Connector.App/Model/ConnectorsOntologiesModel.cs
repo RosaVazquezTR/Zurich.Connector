5bf7e5ed74980c0fb5e4b5a78186d84e7eecf3da
Jose Faustinos <jose.faustinos@thomsonreuters.com>
2023-04-26 17:28:21 -0600

    Add ConnectorsOntologiesModel

---
+++ Zurich.Connector/Zurich.Connector.App/Model/ConnectorsOntologiesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zurich.Connector.App.Model
+{
+    public class ConnectorsOntologiesModel
+    {
+        /// <summary>
+        /// Connector id where Ontologies will be obtained
+        /// </summary>
+        public string ConnectorId { get; set; }
+    }
+}
