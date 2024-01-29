98fa6cb2473ee8923f67b275c8a1bbde204dcda9
Carlos Mejia <Carlos.Mejia@thomsonreuters.com>
2024-01-29 18:51:45 +0000

    Merged PR 14344: Spliting a large document in two and parallel processing them
    Spliting a large document in two and parallel processing them
    
    Related work items: #734674

--- Zurich.Connector/Zurich.Connector.Data/Services/AsposePDFService.cs
+++ Zurich.Connector/Zurich.Connector.Data/Services/AsposePDFService.cs
@@ -23,28 +23,64 @@ namespace Zurich.Connector.Data.Services
             JObject documentObject = new JObject();
             Document document = new Document(documentStream);
             JObject pageText = new JObject();
-            
-            int i = 0;
+            List<Document> documents = new List<Document>();
 
-            if(document.Pages.Count > 0)
+            Document firstHalfDocument = new Document();
+            Document secondHalfDocument = new Document();
+
+            int total = document.Pages.Count;
+            int firstHalf = total / 2;
+            int secondHalf = total - firstHalf;
+
+            if (document.Pages.Count > 0)
             {
-                foreach (var page in document.Pages)
+                for (int i = 1; i < firstHalf; i++)
                 {
-                    TextAbsorber textAbsorber = new TextAbsorber();
-                    page.Accept(textAbsorber);
-                    pageText.Add((i + 1).ToString(), textAbsorber.Text.Replace("\t", "").Replace("\n", "").Replace("\r", ""));
-                    i++;
+                    firstHalfDocument.Pages.Add(document.Pages[i]);
+                }
+                for (int j = firstHalf; j < total + 1; j++)
+                {
+                    secondHalfDocument.Pages.Add(document.Pages[j]);
                 }
             }
+
+            JObject firstHalfObjects = new JObject();
+            JObject secondHalfObjects = new JObject();
+
+            Parallel.Invoke(
+                () => ExtractText(firstHalfDocument, firstHalfObjects, 1),
+                () => ExtractText(secondHalfDocument, secondHalfObjects, secondHalf)
+             );
+
+            firstHalfObjects.Merge(secondHalfObjects, new JsonMergeSettings
+            {
+                MergeArrayHandling = MergeArrayHandling.Union,
+                MergeNullValueHandling = MergeNullValueHandling.Merge
+            });
 
             MemoryStream pdfStream = new MemoryStream();
             document.Save(pdfStream, SaveFormat.Pdf);
             byte[] pdfBytes = pdfStream.ToArray();
             string base64String = Convert.ToBase64String(pdfBytes);
-            documentObject.Add("documentContent", pageText);
+            documentObject.Add("documentContent", firstHalfObjects);
             documentObject.Add("documentBase64", base64String);
 
             return documentObject;
+        }
+
+        public void ExtractText(Document document, JObject jObject, int startingCount)
+        {
+            TextAbsorber textAbsorber = new TextAbsorber();
+
+            for (int i = 0; i < document.Pages.Count; i++)
+            {
+                textAbsorber = new TextAbsorber();
+                document.Pages[i + 1].Accept(textAbsorber);
+                jObject.Add(startingCount.ToString(), textAbsorber.Text.Replace("\t", "").Replace("\n", "").Replace("\r", ""));
+                startingCount++;
+            }
+
+            
         }
     }
 }
